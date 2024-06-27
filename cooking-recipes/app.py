c3b367e268dad48bfd98aa62d0cb88634eede160
RBiebrich <robertbiebrich23@gmail.com>
2024-06-27 18:10:07 -0400

    Add FAQ page and 2 new recipes

--- cooking-recipes/app.py
+++ cooking-recipes/app.py
@@ -3,14 +3,23 @@ from helper import recipes, descriptions, ingredients, instructions
 
 app = Flask(__name__)
 
+
 @app.route('/')
 def index():
     return render_template("index.html", template_recipes=recipes)
+
 
 @app.route('/about')
 def about():
     return render_template("about.html")
 
+
 @app.route("/recipe/<int:id>")
 def recipe(id):
-    return render_template("recipe.html", template_recipe=recipes[id], template_description=descriptions[id], template_ingredients=ingredients[id], template_instructions=instructions[id])
+    return render_template("recipe.html", template_recipe=recipes[id], template_description=descriptions[id],
+                           template_ingredients=ingredients[id], template_instructions=instructions[id])
+
+
+@app.route('/faq')
+def faq():
+    return render_template("faq.html", template_recipes=recipes)
--- cooking-recipes/helper.py
+++ cooking-recipes/helper.py
@@ -1,5 +1,9 @@
-recipes = {1: "fried egg", 2: "buttered toast"}
-descriptions = {1: 'Egg fried in butter', 2: 'Toasted bread spread with butter'}
-ingredients = {1: ['1 pad of butter', '1 Egg', 'A pinch of salt'], 2: ['1 pad of salted butter', '1 slice of bread']}
+recipes = {1: "fried egg", 2: "buttered toast", 3:"kraft easy mac", 4:"rice"}
+descriptions = {1: 'Egg fried in butter', 2: 'Toasted bread spread with butter', 3: 'Kraft macaroni and cheese', 4: ''}
+ingredients = {1: ['1 pad of butter', '1 Egg', 'A pinch of salt'], 2: ['1 pad of salted butter', '1 slice of bread'],
+               3: ['Pasta (from the box)', 'Powdered cheese packet (also from the box)', '1 pad of butter', 'A splash of milk'],
+               4: ['Rice']}
 instructions = {1: {'Step 2': 'Crack the egg into the buttered pan', 'Step 5': 'Serve egg after about a minute and a half', 'Step 1': 'Melt butter in pan over medium-low heat', 'Step 4': 'Flip egg after about a minute and a half', 'Step 3': 'Sprinkle the pinch of salt onto cooking egg',},
-                2: {'Step 3': 'Put the pad of butter on the toasted bread', 'Step 4': 'After a minute spread the melted butter onto the bread', 'Step 1': 'Put the bread in the toaster', 'Step 2': 'Take the toast out of the toaster'}}
+                2: {'Step 3': 'Put the pad of butter on the toasted bread', 'Step 4': 'After a minute spread the melted butter onto the bread', 'Step 1': 'Put the bread in the toaster', 'Step 2': 'Take the toast out of the toaster'},
+                3: {'Step 1': 'Boil 2 cups of water', 'Step 2': 'Add pasta and let cook for 8-10 mins', 'Step 3': 'Drain water from pot and add the powdered cheese, 1 pad of butter, and a splash of milk'},
+                4: {'Step 1': 'Boil an equal amount of water to the amount of rice you are making', 'Step 2': 'Add rice and lower heat. Let simmer for 15-20 mins'}}
